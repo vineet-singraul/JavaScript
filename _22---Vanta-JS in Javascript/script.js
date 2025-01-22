ef2a3c159a8fd1c434608319d8d8a8665d005eb3
Vineet Singraul <Singraulvineet20@gmail.com>
2025-01-22 23:44:08 +0530

    first commit

--- _22---Vanta-JS in Javascript/script.js
+++ _22---Vanta-JS in Javascript/script.js
@@ -28,6 +28,24 @@ VANTA.BIRDS({
 
 
 
+VANTA.BIRDS({
+    el: "#id",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color1: 0x0,
+    colorMode: "lerp",
+    birdSize: 1.10,
+    wingSpan: 40.00,
+    speedLimit: 10.00,
+    separation: 100.00,
+    alignment: 100.00,
+    cohesion: 100.00
+})
 
 
 
