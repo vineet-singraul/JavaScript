dde154010f3bac3fbc5d30dd0a8bb7b1ecce8cd2
Vineet Singraul <singraulvineet943@gmail.com>
2025-01-29 00:03:16 +0530

    Create ex5.js

---
+++ _31---JavaScript--Practice--6/ex5.js
@@ -0,0 +1,8 @@
+let phrase = "I love Python!";
+
+if (phrase.includes("Python")) {
+  let updatedPhrase = phrase.replace("Python", "JavaScript");
+  console.log(updatedPhrase); 
+} else {
+  console.log("The word 'Python' is not in the phrase.");
+}
