4fa9e1874a23c4ffea6d14f74449f97bb8cfe055
Vineet Singraul <singraulvineet943@gmail.com>
2025-01-28 23:57:55 +0530

    Create ex3.js
    lowercase with uppercase check

---
+++ _31---JavaScript--Practice--6/ex3.js
@@ -0,0 +1,13 @@
+let str = "javascript";
+
+if (str === str.toUpperCase()) {
+  console.log("The string is already in uppercase.");
+} else {
+  console.log("Converting to uppercase: " + str.toUpperCase());
+}
+
+if (str === str.toLowerCase()) {
+  console.log("The string is already in lowercase.");
+} else {
+  console.log("Converting to lowercase: " + str.toLowerCase());
+}
