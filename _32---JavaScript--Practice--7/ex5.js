b41ef11b6e6b6bcc38f14e7a13a62f634f5647b7
Vineet Singraul <singraulvineet943@gmail.com>
2025-01-30 21:32:18 +0530

    Update ex5.js

---
+++ _32---JavaScript--Practice--7/ex5.js
@@ -0,0 +1,9 @@
+let sentence = "Hello, welcome to the world of JavaScript!";
+
+let substring = "JavaScript";
+
+if (sentence.includes(substring)) {
+    console.log(`The sentence contains the substring: "${substring}"`);
+} else {
+    console.log(`The sentence does not contain the substring: "${substring}"`);
+}
