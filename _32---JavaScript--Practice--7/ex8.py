ac478c106a97bec920e41ab4c20fa2b7b62784e0
Vineet Singraul <singraulvineet943@gmail.com>
2025-01-30 21:33:16 +0530

    Create ex8.py

---
+++ _32---JavaScript--Practice--7/ex8.py
@@ -0,0 +1,9 @@
+let inputString = "Hello, World!";
+
+if (inputString.length === 0) {
+    console.log("The string is empty.");
+} else if (inputString.length > 10) {
+    console.log("The string is longer than 10 characters.");
+} else {
+    console.log("The string has 10 characters or fewer.");
+}
