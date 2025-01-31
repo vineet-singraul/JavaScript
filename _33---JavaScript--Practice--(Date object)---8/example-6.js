66862d144258da17824889bdd6493ef6275d9342
Vineet Singraul <singraulvineet943@gmail.com>
2025-01-31 21:12:35 +0530

    Create example-6.js

---
+++ _33---JavaScript--Practice--(Date object)---8/example-6.js
@@ -0,0 +1,12 @@
+function getFirstMonday(year, month) {
+    let date = new Date(year, month, 1);
+    while (date.getDay() !== 1) { 
+        date.setDate(date.getDate() + 1);
+    }
+    return date;
+}
+
+for (let month = 0; month < 12; month++) {
+    let firstMonday = getFirstMonday(2024, month);
+    console.log(`First Monday of ${firstMonday.toLocaleString('en-US', { month: 'long', year: 'numeric' })}: ${firstMonday.toDateString()}`);
+}
