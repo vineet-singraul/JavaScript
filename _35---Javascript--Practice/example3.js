47f0ff851087f70d72eba9f2526e6bb028f4135b
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-03 23:09:11 +0530

    Create example3.js
    new code

---
+++ _35---Javascript--Practice/example3.js
@@ -0,0 +1,17 @@
+let num = 2, count = 0;
+while (count < 5) { 
+    let isPrime = true;
+    
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log("Prime:", num);
+        count++;
+    }
+    num++;
+}
