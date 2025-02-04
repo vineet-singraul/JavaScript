18c01f4f50fe8990ccea0ec8fb439f86e1fd28aa
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-04 23:02:11 +0530

    new code

---
+++ _36---Javascript--aPractice/ex1.js
@@ -0,0 +1,9 @@
+let num = 10;
+
+if (num > 0) {
+    console.log("The number is positive.");
+} else if (num < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
