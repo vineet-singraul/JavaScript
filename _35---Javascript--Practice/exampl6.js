e3f26cd00da29f9fb973cb9b0671f044ec6423f0
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-11 23:37:42 +0530

    new commit

---
+++ _35---Javascript--Practice/exampl6.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= 5; j++) {
+        if ((i * j) % 2 === 0) {
+            row += (i * j) + "\t";
+        } else {
+            row += "X\t";
+        }
+    }
+    console.log(row);
+}
+      
