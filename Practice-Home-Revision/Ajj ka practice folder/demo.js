29c371ca35f078b03efcf4e7bcad2e8b63b56f50
Vineet Singraul <singraulvineet943@gmail.com>
2025-05-21 23:56:15 +0530

    new code

---
+++ Practice-Home-Revision/Ajj ka practice folder/demo.js
@@ -0,0 +1,29 @@
+// let arr = [1,2,3,4,5]
+// arr.push(11,12)
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+// arr.shift()
+// console.log(arr);
+
+
+// arr.unshift(1,2)
+// console.log(arr);
+
+
+
+function Data() {
+    const a = 10;
+    console.log(a);
+}
+
+let ClaerAllInterval ;
+// setTimeout(() => {
+   ClaerAllInterval =  setInterval(Data, 2000);
+
+// }, 3000); 
+
+
+clearInterval(ClaerAllInterval)
