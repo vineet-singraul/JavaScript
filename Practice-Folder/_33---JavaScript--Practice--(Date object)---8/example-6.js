b0e571f3857e3881229b9363037f6c520a98417c
Vineet Singraul <singraulvineet943@gmail.com>
2025-06-12 19:21:09 +0530

    new code

--- Practice-Folder/_33---JavaScript--Practice--(Date object)---8/example-6.js
+++ Practice-Folder/_33---JavaScript--Practice--(Date object)---8/example-6.js
@@ -2,7 +2,7 @@ function getFirstMonday(year, month) {
     let date = new Date(year, month, 1); 
     while (date.getDay() !== 1) { 
         date.setDate(date.getDate() + 1);
-    }
+    }    
     return date;
 }
 for (let month = 0; month < 12; month++) {
