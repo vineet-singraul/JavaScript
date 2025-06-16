a2e144d17fb31c2a18daf2db7e7ea7a1bba06009
Vineet Singraul <singraulvineet943@gmail.com>
2025-06-16 12:34:43 +0530

    new code

---
+++ Practice-Home-Revision/Lecture-8--ObjectsInJS/dotAndbracket.js
@@ -0,0 +1,9 @@
+const user = {
+    "name":"Vineet",
+    "age" : 21,
+    "quali-gradu":"B-tech"
+}
+
+
+console.log("Dot Notation : ",user.name);
+console.log("Dot Notation : ",user.quali-gradu);
---
+++ Practice-Home-Revision/Lecture-8--ObjectsInJS/shellowANDdeep.js
@@ -0,0 +1,15 @@
+const user = {
+    "name":"Vineet",
+    "city":"Bhopal",
+    "age":21
+}
+
+console.log("First  : ",user);
+console.log("First  : ",typeof(user));
+console.log("First  : ",user.address);
+
+user.name = "Rahul"
+
+console.log("Second  : ",user);
+console.log("First  : ",typeof(user));
+console.log("First  : ",user.address);
