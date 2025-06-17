f434ad95be578d1ad94390e7c2268bdfef501c55
Vineet Singraul <singraulvineet943@gmail.com>
2025-06-17 13:06:12 +0530

    new code

---
+++ Practice-Home-Revision/Lecture-9--Array-method/ex1.js
@@ -0,0 +1,42 @@
+// let a = [1];
+// let b = [2];
+// let c = [a.concat(b)];
+// console.log(c);
+// // console.log(c[0] === a.concat(b));
+// console.log(c[0]);
+
+
+
+// let a = [1, 2];
+// let b = a.concat();
+// b.push(3);
+// console.log(b);
+
+
+
+// let a = [1];
+// let b = [2];
+// let c = a.concat(b);
+// c[0] = 100;
+// console.log(c);
+
+
+
+
+// let a = [1];
+// let b = [2];
+// let result = a.concat(b, a);
+// result[2] = 999;
+// console.log(result);
+
+
+
+// let a = [[1, 2]];
+// let b = a.concat();
+// b[0][0] = 99;
+// console.log(b);
+
+let a = ["HTML","JS","CSS"]
+let b = ["JAVA","C++","PYTHON"]
+let c = b.copyWithin(0,1,2)
+console.log(c)
