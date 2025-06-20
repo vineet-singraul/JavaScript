c2a396c5ea9fad33f30ca30d2ff48dba0c99db2a
Vineet Singraul <singraulvineet943@gmail.com>
2025-06-20 13:07:05 +0530

    new code

---
+++ Practice-Home-Revision/Lecture-11--Date-Object/date.js
@@ -0,0 +1,14 @@
+
+
+setTimeout(() => {
+    let date = new Date();
+console.log("Year Is : ",date.getFullYear());
+console.log("Month Is : ",date.getMonth()+1);
+console.log("Date Is : ",date.getDate());
+console.log("Day Is : ",date.getDay());
+console.log("Houres Is : ",date.getHours());
+console.log("Minet Is : ",date.getMinutes());
+console.log("Seconds Is : ",date.getSeconds());
+console.log("Milliseconds Is : ",date.getMilliseconds());
+
+}, 1000);
---
+++ Practice-Home-Revision/Lecture-11--Date-Object/stringssDate.js
@@ -0,0 +1,13 @@
+let date = new Date();
+// Full date and time string
+date.toDateString();      // Only date
+date.toTimeString();      // Only time
+date.toISOString();       // ISO 8601 format
+date.toUTCString();       // UTC format
+
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toISOString());
+console.log(date.toUTCString());
+
