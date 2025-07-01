ee7b1542695584795566e5dba01b5f0f5ad940d2
Vineet Singraul <singraulvineet943@gmail.com>
2025-07-01 14:22:41 +0530

    practical more

---
+++ Practice-Home-Revision/Ajj ka practice folder/prac-2/ex7.js
@@ -0,0 +1,8 @@
+// let arr = ["apple","orange","mango","graps"]
+// arr.splice(1,3,"Vineet","Anmol")
+// console.log(arr);
+
+// sort
+let arr = [1,10,30,20,33,10,40,50,27]
+arr.sort()
+console.log(arr);
